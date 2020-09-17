d604b0d25f1e7f4cb1b1c1deab89c47e61185abf
fejzic <dzenana.fejzic@gmail.com>
2020-09-17 15:29:46 +0200

    dovrila sam zadatak 3, iz tutroijala 2

--- src/ba/unsa/etf/rs/zadatak3/Main.java
+++ src/ba/unsa/etf/rs/zadatak3/Main.java
@@ -10,7 +10,28 @@ public class Main {
         System.out.println("Unesite recenicu: ");
         String recenica = scanner.nextLine();
         String najduza_recenica = "";
-        
+        String[] split = recenica.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            String rijec = split[i];
+            if (rijec.length() <= najduza_recenica.length()) {
+                continue;
+            }
+            najduza_recenica = rijec;
 
-    }
+        }
+        String recenica2 = "";
+        String[] s = recenica.split(" ");
+        int i = 0, sLength = s.length;
+        while (true) {
+            if (i >= sLength) break;
+            String rijec2 = s[i];
+            if (rijec2.equals(najduza_recenica)) {
+                rijec2 = rijec2.toUpperCase();
+            }
+            recenica2 += rijec2 + " ";
+            i++;
+        }
+        System.out.println(recenica2);
+       }
+
 }
