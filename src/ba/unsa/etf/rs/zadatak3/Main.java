c455227e773baf1233969713c50e26f2ff6aafc6
fejzic <dzenana.fejzic@gmail.com>
2020-09-17 15:17:32 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rs/zadatak3/Main.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rs.zadatak3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite recenicu: ");
+        String recenica = scanner.nextLine();
+        String najduza_recenica = "";
+        
+
+    }
+}
